963d0f0f7d265400fd68c5986fc5779634d927dc
krak_os <48405896+missedpoints@users.noreply.github.com>
2023-03-29 12:30:27 +0100

    Second commit with temporary values

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,32 @@ fn torque(torque_input: f32, gear_ratio: f32) -> f32
 
 
 
-//5.5; for the wheel's bore length.
-// plan: 5 > 6 > 7 > 10 teeth
-
+//5.5; for the wheel's bore length. 
+// plan: 5 > 6 > 7 > 10 teeth >> ended up using something else.
+fn time_to_reach_top(
+    weight: f32,
+    torque: f32,
+    speed: f32,
+    gear_ratio: f32,
+    distance: f32
+) -> f32
+{
+    let force = weight * 9.81; // 9.81 in this context is G.
+    let torque = torque * gear_ratio; // 0.044 kg/cm
+    let speed = speed * gear_ratio;
+    let time = distance / speed;
+    return time;
+}
 fn main()
 {
     
     const PI: f32 = 3.14159265359;
     const max_torque: f32 = 1.18; // 2.16 Ncm; 1.18 Ncm
     const max_speed: f32 = 9.7*(10 as f32); // RPM
+    const weight: f32 = 4.4;
+    const motor_torque: f32 = 0.5;
+    const motor_speed: f32 = 9600.0;
 
     println!("Max torque: {} Ncm", torque(max_torque, gear_ratio(5.0, 6.0)));
-    println!("Gear ratio: {}", gear_ratio(5.0, 6.0));
+    println!("Gear ratio: {}", gear_ratio(5.0, 6.0)); // starting to think I should've used Python instead.
 }
